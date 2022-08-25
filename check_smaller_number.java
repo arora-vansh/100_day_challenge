32aac7ce6c96aebbd34ffbb4966b4792465d20c1
Vansh <vansharora2204@gmail.com>
2022-08-25 18:47:50 +0530

    day-2

--- check_smaller_number.java
+++ check_smaller_number.java
@@ -9,9 +9,9 @@ public class check_smaller_number {
         num2=scan.nextInt();
         scan.close();
         if (num1 > num2)
-        System.out.println(num1 + " is larger");
+        System.out.println(num2 + " is smaller");
         else if(num2 > num1)
-        System.out.println(num2 + " is larger");
+        System.out.println(num1 + " is smaller");
         else
         System.out.println("The entered numbers are equal");
     }
