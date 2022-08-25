7f4b0c828eb3c3bf62a44c2cc3546c21833062bd
Vansh <vansharora2204@gmail.com>
2022-08-25 17:49:18 +0530

    day-2

---
+++ check_odd_or_even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class check_odd_or_even {
+    public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+        int num;
+        System.out.println("Enter the number:");
+        num=scan.nextInt();
+        if (num % 2 == 0)
+        System.out.println("Given number is even");
+        else
+        System.out.println("Given number is odd");
+    }
+
+
+    
+}
