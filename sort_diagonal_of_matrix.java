69b47cefb598528defffef3a299ea8819605b2d5
Vansh <vansharora2204@gmail.com>
2022-11-13 00:05:39 +0530

    day-80

--- sort_diagonal_of_matrix.java
+++ sort_diagonal_of_matrix.java
@@ -1,36 +1,31 @@
 import java.util.Scanner;
 public class sort_diagonal_of_matrix 
-{
-
-
-    public static void main(String[] args)
+{    public static void main(String[] args)
     {
         try(Scanner scan = new Scanner(System.in))
         {
-
-        int i,j,n,m,temp;
-        int[][] a = new int[10][10]; 
-        n=scan.nextInt();
-        m=scan.nextInt();
-        for(i=0 ; i<n ; i++){
-                for(j=0 ; j<m ; j++)
-                {
-                a[i][j] = scan.nextInt();
-                }
-            }
+            int i,j,n,m,temp;
+            int[][] a = new int[10][10];
+            System.out.print("Enter the number of rows in the matrix:"); 
+            n=scan.nextInt();
+            System.out.print("Enter the number of columns in the matrix:");
+            m=scan.nextInt();
             for(i=0 ; i<n ; i++)
             {
                 for(j=0 ; j<m ; j++)
                 {
-                    if(i==j)
+                    a[i][j] = scan.nextInt();
+                }
+            }
+            for(i=0 ; i<n ; i++)
+            {
+                for(j=i+1 ; j<n ; j++)
+                {
+                    if(a[i][i]>a[j][j])
                     {
-                        for(int k=1 ; k<n ; k++){
-                        if(a[i][j] > a[i+k][j+k])
-                        {
-                            temp = a[i][j];
-                            a[i][j] = a[i+1][j+1];
-                            a[i+k][j+k] = temp;
-                        }
+                        temp = a[i][i];
+                        a[i][i] = a[j][j];
+                        a[j][j] = temp;
                     }
                 }
             }
@@ -44,5 +39,4 @@ public class sort_diagonal_of_matrix
             }
         }
     }
-}
 }
