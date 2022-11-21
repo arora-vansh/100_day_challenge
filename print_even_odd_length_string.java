827d991fd39836b3488a5b9da45159a0d0755be9
Vansh <vansharora2204@gmail.com>
2022-11-21 23:08:22 +0530

    day-86

--- print_even_odd_length_string.java
+++ print_even_odd_length_string.java
@@ -5,21 +5,19 @@ public class print_even_odd_length_string
     {
         Scanner scan = new Scanner(System.in);
         String s;
-        String[] word;
         System.out.print("Enter the String:");
         s = scan.nextLine();
         scan.close();
-        word = s.split(" ");
         System.out.print("The even length words are ");
-        for(int i=0 ; i<s.length() ; i++)
+        for(String word : s.split(" "))
         {
-            if(word.length % 2 == 0)
+            if(word.length() % 2 == 0)
             System.out.print(word);
         } 
         System.out.print("The odd length words are ");
-        for(int i=0 ; i<s.length() ; i++)
+        for(String word : s.split(" "))
         {
-            if(word.length % 2 != 0)
+            if(word.length() % 2 != 0)
             System.out.print(word);
         }
     } 
